98ac12ad9f7d0ad47d09200b1a010d7fd2eb178b
최영준 <44596010+speed5458@users.noreply.github.com>
2022-06-03 23:37:05 +0900

    JPA 연결성공

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +18,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testmember() throws Exception{
         //given
         Member member = new Member();
@@ -29,6 +31,7 @@ class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
     }
 
