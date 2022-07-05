f9a4679e683b98877c5a31b079b49a010180abfe
최영준 <44596010+speed5458@users.noreply.github.com>
2022-07-05 14:45:19 +0900

    회원 도메인 개발 - 회원 서비스 개발
    MemberRepository 작성
    MemberService 작성
    
    @RequiredArgsConstructor 어노테이션을 통해 생성자 인젝션을 간단하게 해결
    Repository의 @PersistenceContext 어노테이션도 스프링부투에서 지원을 해주기때문에 생략 가능해서 사용 가능하다.

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional //중요
+@RequiredArgsConstructor //final 붙은 변수에 생성자 생성해준다.
+public class MemberService {
+
+    //fianl 넣어주면 컴파일시점에 인젝션 해줬나 안해줬나 확인이 가능하다.
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        //실무에서는 DB쪽 제약조건을 하나 더 걸어서 쓰자.
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
