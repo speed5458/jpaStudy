2bd696907ee246d4f5e7675800dcad6c7070c8a8
최영준 <44596010+speed5458@users.noreply.github.com>
2022-07-06 10:35:12 +0900

    상품도메인 개발 - 상품엔티티개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,30 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //==비즈니스 로직==//
+    /**
+     *
+     * stock 증가
+     */
+
+
+    //setter 를 써서 직접 stockQuantity 를 직접 수정하는게 아니라 핵심비즈니스 로직을 가지고 수정을 해줘야 한다.
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     *
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
